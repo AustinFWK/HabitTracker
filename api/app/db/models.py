6d15113bc29a6416ed9e3ea51c909a89335f8504
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-07-29 10:54:00 -0400

    add email column, and make username required

--- api/app/db/models.py
+++ api/app/db/models.py
@@ -6,7 +6,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String(255), unique=True, index=True, nullable=False)
 
     daily_habits = relationship("Daily Habits", back_populates="owner")
 
