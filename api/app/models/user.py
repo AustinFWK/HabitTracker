227f0fcfdd3408b79b0c95084659effa3d8c8f3c
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-10-12 18:07:26 -0400

    remove user create schema, remove email and username from user db model

--- api/app/models/user.py
+++ api/app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer
 from app.db.database import Base
 from sqlalchemy.orm import relationship
 
@@ -6,8 +6,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
-    email = Column(String(255), unique=True, index=True, nullable=False)
     #birthdate = Column(Integer, nullable=True)
 
     daily_entries = relationship("DailyEntry", back_populates="owner")
