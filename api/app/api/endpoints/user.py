c93663e3ac62e17241dd7950c3d548ef3fcda309
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-10-12 19:26:27 -0400

    remove unnecessary user endpoints

--- api/app/api/endpoints/user.py
+++ api/app/api/endpoints/user.py
@@ -9,15 +9,6 @@ router = APIRouter(
     tags=["user"]
 )
 
-@router.post("/", response_model= UserRead)
-def create_user(user: UserCreate, session = Depends(get_session)) -> User:
-    db_user = User(**user.model_dump())
-    session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
-    return db_user
-
-
 #delete user endpoint
 @router.delete("/{user_id}", response_model=None)
 def delete_user(user_id: int, session = Depends(get_session)) -> User:
@@ -36,16 +27,4 @@ def get_user(user_id: int, session = Depends(get_session)) -> User:
         raise HTTPException(status_code=404, detail="User does not exist")
     return db_user
 
-#update user endpoint
-@router.put("/{user_id}", response_model=UserRead)
-def update_user(user_id: int, user: UserUpdate, session = Depends(get_session)) -> User:
-    db_user = session.query(User).filter(User.id == user_id).first()
-    if not db_user:
-        raise HTTPException(status_code=404, detail="User does not exist")
-    
-    update_data = user.model_dump(exclude_unset=True)
-    for key, value in update_data.items():
-        setattr(db_user, key, value)
-    session.commit()
-    session.refresh(db_user)
-    return db_user
+
--- api/app/models/user.py
+++ api/app/models/user.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import relationship
 class User(Base):
     __tablename__ = "users"
 
+    #i believe clerk provides its own unique id for each user, so i have to check how to integrate that here
     id = Column(Integer, primary_key=True, index=True)
-    #birthdate = Column(Integer, nullable=True)
 
     daily_entries = relationship("DailyEntry", back_populates="owner")
 
