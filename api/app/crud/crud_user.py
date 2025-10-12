cef20ee4fcc9c0aa3f1be6887e2c45a75fa54706
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-10-12 18:04:45 -0400

    delete crud user file for now

--- api/app/crud/crud_user.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session
-from api.app.models.user import User
-from api.app.schemas.user import UserCreate
-
-def create_user(db: Session, user: UserCreate):
-    db_user = User(username=user.username, email=user.email, birthdate=user.birthdate)
