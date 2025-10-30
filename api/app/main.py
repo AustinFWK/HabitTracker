3061f3d2bf3f340b714062efde8ee085f77de3f1
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-10-30 12:18:05 -0400

    add cors to main.py

--- api/app/main.py
+++ api/app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .api.endpoints import user, entry
 from app.db.database import engine, Base
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+
+origins = [
+    "http://localhost:5173",  # Frontend (Vite)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(user.router)
