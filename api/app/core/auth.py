279249df6f385a2bcc95cdeb4c652372ff34e18d
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-11-01 21:50:59 -0400

    add get jwks function

--- api/app/core/auth.py
+++ api/app/core/auth.py
@@ -1,7 +1,20 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import jwt, JWTError
-import requests
+from functools import lru_cache
 from typing import Dict
+import os
+import requests
 
-requests()
+security = HTTPBearer()
+
+@lru_cache(maxsize=1)
+def get_jwks() -> Dict:
+    jwks_clerk_url = os.getenv("CLERK_JWKS_URL")
+    if not jwks_clerk_url:
+        raise ValueError("CLERK_JWKS_URL environment variable not set")
+    
+    response = requests.get(jwks_clerk_url)
+    response.raise_for_status()
+    return response.json()
+
