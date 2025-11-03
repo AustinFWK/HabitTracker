5b9c8604910ee8f0660e3a66b6b386371c739db8
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-11-02 19:46:31 -0500

    add auth to entry endpoint

--- api/app/api/endpoints/entry.py
+++ api/app/api/endpoints/entry.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from app.schemas.entry import EntryRead, EntryCreate, EntryUpdate
 from app.models.entry import DailyEntry
 from app.db.database import get_session
+from app.core.auth import get_current_user
+from typing import Dict
 
 router = APIRouter(
     prefix="/entry",
@@ -9,8 +11,9 @@ router = APIRouter(
 )
 
 @router.post("/", response_model=EntryRead)
-def create_entry(entry: EntryCreate, session = Depends(get_session)) -> DailyEntry:
-    db_entry = DailyEntry(**entry.model_dump())
+def create_entry(entry: EntryCreate, session = Depends(get_session), current_user: Dict = Depends(get_current_user)) -> DailyEntry:
+    clerk_user_id = current_user("sub")
+    db_entry = DailyEntry(entry=entry.entry, clerk_user_id=clerk_user_id)
     session.add(db_entry)
     session.commit()
     session.refresh(db_entry)
