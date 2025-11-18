5905868500a9f34a46bdbddc1e3e8b57cac8e00d
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-11-17 23:26:43 -0500

    add basic navbar and routes to app

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -4,18 +4,31 @@ import {
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import History from "./pages/History";
 
 export default function App() {
   return (
-    <>
-      <header>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
-    </>
+    <BrowserRouter>
+      <>
+        <header>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+            <nav>
+              <Link to="/">Home</Link>
+              <Link to="/history">History</Link>
+            </nav>
+          </SignedIn>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
+      </>
+    </BrowserRouter>
   );
 }
