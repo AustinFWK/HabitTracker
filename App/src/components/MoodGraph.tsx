cf3dae3a50aea8ad09887f88fc8a9a3a9856eb7d
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-12-01 21:24:33 -0500

    add try catch block for fetching data from API

--- App/src/components/MoodGraph.tsx
+++ App/src/components/MoodGraph.tsx
@@ -17,4 +17,37 @@ function MoodGraph() {
   const [error, setError] = useState("");
 
   const { getToken } = useAuth();
+
+  useEffect(() => {
+    const fetchCheckIns = async () => {
+      setIsLoading(true);
+      setError("");
+
+      try {
+        const token = await getToken({ template: "backend" });
+        const response = await fetch("http://127.0.0.1:8000/check_in", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch check-ins");
+        }
+
+        const data = await response.json();
+        setCheckIns(data);
+      } catch {
+        setError("Error fetching check-in data");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchCheckIns();
+  }, []);
+
+  return <div></div>;
 }
+
+export default MoodGraph;
