57340d280a49e6e56fcc3df3730a59c5f24a5632
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-12-02 20:10:03 -0500

    add useEffect to check if user has logged for the day

--- App/src/pages/Home.tsx
+++ App/src/pages/Home.tsx
@@ -3,9 +3,38 @@ import DailyCheckInForm from "../components/DailyCheckInForm";
 import MoodGraph from "../components/MoodGraph";
 import { useState } from "react";
 import { Button } from "@mui/material";
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect } from "react";
 
 function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [hasCheckedIn, setHasCheckedIn] = useState(false);
+
+  const { getToken } = useAuth();
+
+  useEffect(() => {
+    const getTodayCheckIn = async () => {
+      const token = await getToken({ template: "backend" });
+      const todaysDate = new Date().toISOString().split("T")[0]; // "2025-12-02"
+      const response = await fetch(
+        `http://127.0.0.1:8000/check_in/${todaysDate}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        setHasCheckedIn(true);
+      }
+      if (response.status === 404) {
+        setHasCheckedIn(false);
+      }
+    };
+    getTodayCheckIn();
+  }, [getToken]);
 
   return (
     <div>
