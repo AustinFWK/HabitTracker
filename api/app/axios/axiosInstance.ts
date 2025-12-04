844e8883fc315df0004570007e0f97f16757e546
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-12-04 16:15:52 -0500

    add axios instance file

---
+++ api/app/axios/axiosInstance.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+let getTokenFunction: (() => Promise<string | null>) | null = null;
+
+export const setAuthTokenGetter = (
+  tokenGetter: () => Promise<string | null>
+) => {
+  getTokenFunction = tokenGetter;
+};
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    if (getTokenFunction) {
+      const token = await getTokenFunction();
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
