cd202306acff5c2d9a5c1a076a5128533654e016
Austin Winger-Kailer <145706646+AustinFWK@users.noreply.github.com>
2025-12-08 09:34:01 -0500

    create StreakStats interface, add if statement for no check-ins, sort date in order

---
+++ App/src/utils/streakTracker.ts
@@ -0,0 +1,27 @@
+import dayjs from "dayjs";
+import type { CheckInData } from "../../../api/app/api/services/checkInService";
+
+export interface StreakStats {
+  currentStreak: number;
+  longestStreak: number;
+  totalCheckIns: number;
+  lastCheckInDate: string | null;
+  streakStartDate: string | null;
+  isOngoingToday: boolean;
+}
+
+export function calculateStreakStats(checkIns: CheckInData[]): StreakStats {
+  if (checkIns.length === 0) {
+    return {
+      currentStreak: 0,
+      longestStreak: 0,
+      totalCheckIns: 0,
+      lastCheckInDate: null,
+      streakStartDate: null,
+      isOngoingToday: false,
+    };
+  }
+  const sortedDates = checkIns
+    .map((checkIns) => dayjs(checkIns.date))
+    .sort((a, b) => a.valueOf() - b.valueOf());
+}
